5c48b0e9f62135da3235b321e93a8229fe82c09f
crew <crew@MacBookPro.taila85d4c.ts.net>
2026-04-01 21:47:22 +0700

    update

--- src/pages/ToDoList.jsx
+++ src/pages/ToDoList.jsx
@@ -5,7 +5,7 @@ import style from './ToDoList.module.css';
 
 function ToDoList() {
 
-    const [tasks, setTasks] = useState(["choi voi pe tram", "task2", "task3"]);
+    const [tasks, setTasks] = useState(["task1", "task2", "task3"]);
     const [newTask, setNewTask] = useState('');
 
     function handleInputChange(event) {
